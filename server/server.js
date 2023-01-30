066b0e5ffd83ae903c16aaa2e8b432ad571c945e
Debora Correia <deboracorreia@Deboras-MacBook-Air.local>
2023-01-29 22:14:44 -0800

    schema initial setup

--- server/server.js
+++ server/server.js
@@ -2,21 +2,29 @@ import { ApolloServerPluginDrainHttpServer } from "apollo-server-core";
 import { ApolloServer } from "apollo-server-express";
 import express from "express";
 import http from 'http'
+import { resolvers, typeDefs } from "./src/schema";
 
-const startApolloServer = async () => {
+const startApolloServer = async (typeDefs, resolvers) => {
     const app = express ()
 
     const httpServer = http.createServer(app)
 
+
     const server = new ApolloServer({
+        typeDefs,
+        resolvers,
+        //plugin designed for use with express middleware built on top of node http.server - recommended to use to ensure that server shuts down gracefully
+        //plugin not necessary with satrtStandaloneServer
         plugins: [ApolloServerPluginDrainHttpServer({httpServer})]
     })
 
-    await server.start
+    await server.start()
 
     server.applyMiddleware({ app })
 
-    await new Promise(resolve => httpServer.listen({ port: 400 }, resolve))
+    await new Promise(resolve => httpServer.listen({ port: 4000 }, resolve))
 
-    console.log()
+    console.log(`🚀 server ready at http://localhost:4000${server.graphqlPath}`)
 }
+
+startApolloServer(typeDefs, resolvers)
---
+++ server/src/schema.js
@@ -0,0 +1,39 @@
+import { gql } from 'apollo-server-express'
+
+const contactsArray =[
+    {
+        id: '1',
+        firstName: 'Paul',
+        lastName: 'Lam'
+    },
+    {
+        id: '2',
+        firstName: 'Elon',
+        lastName: 'Musk'
+    },
+    {
+        id: '3',
+        firstName: 'Steve',
+        lastName: 'Jobs'
+    }
+]
+
+const typeDefs = gql`
+    type Contact {
+        id: String!
+        firstName: String
+        lastName: String
+    }
+
+    type Query {
+        contacts: [Contact]
+    }
+`
+
+const resolvers = {
+    Query: {
+        contacts: () => contactsArray
+    }
+}
+
+export {typeDefs, resolvers}
