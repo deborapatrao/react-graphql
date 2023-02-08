89ea52b86dd1413e19f6affb09d3f3bedf2cd22b
deborapatrao <deborapcorreia@gmail.com>
2023-02-08 00:09:09 -0800

    Add form validation

--- client/src/components/forms/AddContact.js
+++ client/src/components/forms/AddContact.js
@@ -1,14 +1,20 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { v4 as uuidv4 } from 'uuid'
-import { Form, Input } from 'antd'
+import { Button, Form, Input } from 'antd'
 
 const AddContact = () => {
     const [id] = useState(uuidv4())
+    const [form] = Form.useForm()
+    const[,forceUpdate] = useState()
 
+    useEffect(() => {
+        forceUpdate([])
+    }, [])
 
 return(
     <Form 
     name='add-contact-form'
+    form={form}
     layout='inline'
     size='large'
     style={{marginBottom: '40px'}}>
@@ -18,6 +24,15 @@ return(
 
         <Form.Item name='LasttName' rules={[{required:true, message:'Please input last name!'}]}>
             <Input placeholder='i.e. Smith'/>
+        </Form.Item>
+
+        <Form.Item shouldUpdate={true}>
+            {() => (
+                <Button 
+                type='primary'
+                htmlType='submit'
+                disabled={!form.isFieldsTouched(true) || form.getFieldsError().filter(({ errors }) => errors.length).length}>Add Contact</Button>
+            )}
         </Form.Item>
     </Form>
 )
